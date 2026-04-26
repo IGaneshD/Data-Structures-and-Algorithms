0f85661b9220280b8efb49aaacb6db0a163c7268
Ganesh Deshmukh <rajdhani.blog@gmail.com>
2026-04-26 21:02:53 +0530

    day 6: 26th Apr 2026-Golang-pattern14

--- StriverDSASheet/Learn the Basics/2. Build Up logical Thinking/pattern14.go
+++ StriverDSASheet/Learn the Basics/2. Build Up logical Thinking/pattern14.go
@@ -6,5 +6,16 @@ import (
 
 
 func main(){
-	fmt.Printf("%c", 65)
+	var endCharacter int;
+	fmt.Print("Enter the end Character:")
+
+	fmt.Scanf("%c", &endCharacter)
+
+	for  i  := 0; i<= int(endCharacter) - 65; i++ {
+		for j := 65; j<=65+i; j++{
+			fmt.Printf("%c", j)
+		}
+		fmt.Println()
+	}
+	
 }
